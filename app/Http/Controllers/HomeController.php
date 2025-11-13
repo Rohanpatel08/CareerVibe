78ce76517e0e5052ccd46727ce275a5d13825c66
Rohanpatel08 <raj.bangoriya08@gmail.com>
2025-11-13 23:32:05 +0530

    search added in home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,11 +11,13 @@ class HomeController extends Controller
     public function index()
     {
         $categories = JobCategory::where('status', 1)->orderBy('category', 'ASC')->take(8)->get();
+        $allCategories = JobCategory::where('status', 1)->orderBy('category', 'ASC')->get();
         $featured_jobs = CareerJob::where('status', 1)->where('isFeatured', 1)->orderBy('created_at', 'DESC')->take(6)->get();
         $latest_jobs = CareerJob::where('status', 1)->orderBy('created_at', 'DESC')->take(6)->get();
         // dd($featured_jobs);
         return view('front.home', [
             'categories' => $categories,
+            'allCategories' => $allCategories,
             'featured_jobs' => $featured_jobs,
             'latest_jobs' => $latest_jobs
         ]);
--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -17,30 +17,33 @@
     <section class="section-1 py-5 ">
         <div class="container">
             <div class="card border-0 shadow p-5">
-                <div class="row">
-                    <div class="col-md-3 mb-3 mb-sm-3 mb-lg-0">
-                        <input type="text" class="form-control" name="search" id="search" placeholder="Keywords">
-                    </div>
-                    <div class="col-md-3 mb-3 mb-sm-3 mb-lg-0">
-                        <input type="text" class="form-control" name="search" id="search" placeholder="Location">
-                    </div>
-                    <div class="col-md-3 mb-3 mb-sm-3 mb-lg-0">
-                        <select name="category" id="category" class="form-control">
-                            <option value="">Select a Category</option>
-                            <option value="">Engineering</option>
-                            <option value="">Accountant</option>
-                            <option value="">Information Technology</option>
-                            <option value="">Fashion designing</option>
-                        </select>
-                    </div>
+                <form action="{{ route('jobs')}}" method="GET">
 
-                    <div class=" col-md-3 mb-xs-3 mb-sm-3 mb-lg-0">
-                        <div class="d-grid gap-2">
-                            <a href="jobs.html" class="btn btn-primary btn-block">Search</a>
+                    <div class="row">
+                        <div class="col-md-3 mb-3 mb-sm-3 mb-lg-0">
+                            <input type="text" class="form-control" name="keyword" id="keyword" placeholder="Keywords">
+                        </div>
+                        <div class="col-md-3 mb-3 mb-sm-3 mb-lg-0">
+                            <input type="text" class="form-control" name="location" id="location" placeholder="Location">
+                        </div>
+                        <div class="col-md-3 mb-3 mb-sm-3 mb-lg-0">
+                            <select name="category" id="category" class="form-control">
+                                <option value="">Select a Category</option>
+                                @foreach ($allCategories as $category)
+                                    <option value="{{ $category->id }}">{{ $category->category }}</option>
+                                @endforeach
+                            </select>
                         </div>
 
+                        <div class=" col-md-3 mb-xs-3 mb-sm-3 mb-lg-0">
+                            <div class="d-grid gap-2">
+                                {{-- <a href="jobs.html" class="btn btn-primary btn-block">Search</a> --}}
+                                <button type="submit" class="btn btn-primary btn-block">Search</button>
+                            </div>
+
+                        </div>
                     </div>
-                </div>
+                </form>
             </div>
         </div>
     </section>
@@ -53,7 +56,7 @@
                     @foreach ($categories as $category)
                         <div class="col-lg-4 col-xl-3 col-md-6">
                             <div class="single_catagory">
-                                <a href="jobs.html">
+                                <a href="{{ route('jobs') . '?category=' . $category->id }}">
                                     <h4 class="pb-2">{{ $category->category }}</h4>
                                 </a>
                                 <p class="mb-0"> <span>50</span> Available position</p>
